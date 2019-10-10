6c9d947713a13ce0f5cc123dfa6885c509c51d27
aslan66 <56066373+aslan66@users.noreply.github.com>
2019-10-10 17:40:27 +0900

    create home.blade.app

---
+++ resources/views/home.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>HOME</title>
+  <!-- <link href="{{ asset('/css/app.css') }}" rel="stylesheet"> -->
+  <!-- Fonts -->
+  <link href='//fonts.googleapis.com/css?family=Roboto:400,300' rel='stylesheet' type='text/css'>
+
+  <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+  <script src="//cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.1/js/bootstrap.min.js"></script>
+</head>
+<body>
+
+  <!-- Scripts -->
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -15,4 +15,4 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::resource('todo', 'HomeController');
+Route::resource('home', 'HomeController');
