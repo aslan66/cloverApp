4adc33078e6706130bde7a3447729462244cee69
aslan66 <56066373+aslan66@users.noreply.github.com>
2019-10-15 18:48:42 +0900

    add clv image

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,15 +5,51 @@
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <title>HOME</title>
-  <!-- <link href="{{ asset('/css/app.css') }}" rel="stylesheet"> -->
+  <link href="{{ asset('/css/app.css') }}" rel="stylesheet">
   <!-- Fonts -->
   <link href='//fonts.googleapis.com/css?family=Roboto:400,300' rel='stylesheet' type='text/css'>
+  <link href="https://fonts.googleapis.com/css?family=Playfair+Display|Spectral" rel="stylesheet">
+  <link href="https://fonts.googleapis.com/css?family=Caveat rel="stylesheet">
 
   <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
   <script src="//cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.1/js/bootstrap.min.js"></script>
 </head>
 <body>
 
-  <!-- Scripts -->
+<nav>
+  <ul>
+    <li class=”current”><a href=”#”>Home</a></li>
+    <li><a href=”#”>News</a></li>
+    <li><a href=”#”>About</a></li>
+    <li><a href=”#”>Access</a></li>
+    <li><a href=”#”>Blog</a></li>
+  </ul>
+</nav>
+
+
+<!-- <ul class="cb-slideshow">
+    <li>
+        <span>Image 01</span>
+		<div><h3 class="key_title">Clover Official</i></h3></div>
+    </li>
+    <li> <span>Image 02</span>
+         <div><h3 class="key_title">Clover Official</i></h3></div>
+    </li>
+    <li>
+        <span>Image 03</span>
+        <div><h3 class="key_title">Clover Official</h3></div>
+    </li>
+    <li> <span>Image 04</span>
+         <div><h3 class="key_title">Clover Official</h3></div>
+    </li>
+    <li>
+        <span>Image 05</span>
+        <div><h3 class="key_title">Clover Official</h3></div>
+    </li>
+    <li> <span>Image 06</span>
+        <div><h3 class="key_title">Clover Official</h3></div>
+    </li>
+</ul> -->
+
 </body>
 </html>
